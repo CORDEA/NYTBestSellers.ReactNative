01d0b1a270291957186c275f0aec3f2316397838
Yoshihiro Tanaka <contact@cordea.jp>
2019-08-17 20:04:19 +0900

    set headers

--- ApiClient.ts
+++ ApiClient.ts
@@ -1,10 +1,18 @@
 export class ApiClient {
-    private static readonly BASE_URL = "https://api.nytimes.com/"
-    private static readonly BASE_PATH = "svc/books/v3"
-    private static readonly GET_LISTS_PATH = ApiClient.BASE_URL + ApiClient.BASE_PATH + "/lists.json"
-    private static readonly GET_LIST_NAMES_PATH = ApiClient.BASE_URL + ApiClient.BASE_PATH + "/lists/names.json"
+    private static readonly BASE_URL = "https://api.nytimes.com/";
+    private static readonly BASE_PATH = "svc/books/v3";
+    private static readonly GET_LISTS_PATH = ApiClient.BASE_URL + ApiClient.BASE_PATH + "/lists.json";
+    private static readonly GET_LIST_NAMES_PATH = ApiClient.BASE_URL + ApiClient.BASE_PATH + "/lists/names.json";
+
+    private headers() {
+        let headers = new Headers();
+        headers.append("api-key", "");
+        return headers
+    }
 
     getLists(list: string): Promise<Response> {
-        return fetch(ApiClient.GET_LISTS_PATH)
+        return fetch(ApiClient.GET_LISTS_PATH, {
+            headers: this.headers()
+        })
     }
 }

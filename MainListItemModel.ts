fdeb375e89f8ce45dcd2800550126ddf74003aae
Yoshihiro Tanaka <contact@cordea.jp>
2019-08-18 12:11:13 +0900

    define item model

---
+++ MainListItemModel.ts
@@ -0,0 +1,21 @@
+import {ListsResponse} from "./response/ListsResponse";
+
+class MainListItemModel {
+    title: string;
+    description: string;
+    position: number;
+
+    constructor(title: string, description: string, position: number) {
+        this.title = title;
+        this.description = description;
+        this.position = position;
+    }
+
+    static from(response: ListsResponse): MainListItemModel[] {
+        return response
+            .results
+            .map(response => response.book_details[0])
+            .filter(response => response != null)
+            .map((response, index) => new MainListItemModel(response.title, response.description, index))
+    }
+}
